4df6e2d36310012f8947664f5a0221df0a8eb22d
slyhne <soeren@zurf.dk>
2013-02-20 20:39:09 +0100

    Added Bitcoins transfers

--- languages/en.php
+++ languages/en.php
@@ -15,6 +15,7 @@
 	'donation:show:everyone' => 'Show all',
 	'donation:desc' => "Help us keep %s running.",
 	'donation:paypal' => "<strong>Via Paypal:</strong>",
+	'donation:bitcoin' => "<strong>Via Bitcoin:</strong>",
 	'donation:banktransfer' => "<strong>Via banktransfer:</strong>",
 	'donation:latest' => "<strong>Latest Donators:</strong>",
 	'donation:donator' => 'Contributer to %s',
@@ -25,7 +26,9 @@
 	'donation:none' => 'No donators to display',
 
 	// Plugin settings
-	'donation:paypal_code' => "Insert Paypal code here:",
+	'donation:paypal_code' => "Optional: Insert Paypal code here:",
+	'donation:bitcoin_code' => "Optional: Insert Bitcoin code here (<i>bitcoin:</i> and <i>?label</i> codes will be added for links by this plugin.):",
+	'donation:bitcoin_label' => "Optional: Insert Bitcoin label here:",
 	'donation:bank_account' => "Optional: A bank account number for bank transfers:",
 	'donation:bank_account:text' => "Transfer to account:<br><b>%s</b><br>please state your username on the transfer.",
 	'donation:num_display' => 'Number of donators to display:',
