01ee5f71ffd21748bbb8c3978b5740a1d6111477
slyhne <soeren@zurf.dk>
2013-02-20 21:38:08 +0100

    Update views/default/donation/sidebar.php
    Changed from module aside to featured

--- views/default/donation/sidebar.php
+++ views/default/donation/sidebar.php
@@ -5,5 +5,5 @@
 
 $body = elgg_view('donation/donation');
 $title = elgg_echo('donation:title', array(elgg_get_config('sitename')));
-echo elgg_view_module('aside', $title, $body);
+echo elgg_view_module('featured', $title, $body);
 
